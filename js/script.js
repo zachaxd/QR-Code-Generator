5a96d4d76e7b03f88574a967783633c097847eb5
zacha <zachand9@gmail.com>
2022-08-17 18:15:56 -0500

    Updated style

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ const clearUI = () => {
 const createSaveBtn =(saveUrl) => {
     const link = document.createElement('a');
     link.id = 'save-link';
-    link.classList = 'bg-red-500 hover:bg-red-700 text-white font-bold p-8 rounded w-1/3 m-auto my-5';
+    link.classList = 'bg-lightPurple hover:lightPurple/80 text-white font-bold p-12 rounded w-24 m-auto';
     link.href = saveUrl;
     link.download = 'qrcode';
     link.innerHTML = 'Save Image';
